d8fefeb5701cc484c985e6b4aa1ee08e906624de
Julian Aburrow <vdub1992@hotmail.co.uk>
2026-06-23 13:29:12 +0100

    Initial solution commit with UI, Core, Tests and .gitignore

---
+++ CheckoutKata.Core/Checkout.cs
@@ -0,0 +1,14 @@
+﻿namespace CheckoutKata.Core;
+
+public class Checkout
+{
+    private int _total = 0;
+
+    public void Scan(string sku)
+    {
+        if (sku == "A")
+            _total += 50;
+    }
+
+    public int Total() => _total;
+}
---
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,21 @@
+﻿namespace CheckoutKata.Tests;
+
+public class CheckoutTests
+{
+    [Fact]
+    public void Scanning_single_A_returns_50()
+    {
+        var checkout = new Checkout();
+        checkout.Scan("A");
+        Assert.Equal(50, checkout.Total());
+    }
+
+    [Fact]
+    public void Scanning_two_As_returns_100()
+    {
+        var checkout = new Checkout();
+        checkout.Scan("A");
+        checkout.Scan("A");
+        Assert.Equal(100, checkout.Total());
+    }
+}
